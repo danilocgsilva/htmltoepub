ef794c561af84875c182a7c2c0c602ceabe14171
Danilo <danilocgsilva@gmail.com>
2015-02-22 22:31:41 -0300

    Start the buillding class

--- lib/xml_element.php
+++ lib/xml_element.php
@@ -1,19 +1,7 @@
 <?php
 
 class xml_element {
-  public $name = "";
+  public $name = array();
   public $attributes = array();
-  public $xmlstring = "";
   public $child = array();
-
-  public function print_xml() {
-    print("<" . $name);
-    if (count($this->$attributes) != 0) {
-      print " ";
-      foreach($this->$attributes as key => $attribute) {
-        print $key;
-      }
-      print " ";
-    }
-  }
 }
