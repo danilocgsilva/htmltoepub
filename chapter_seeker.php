aedb061558a8251c7cd4ebd9f96c9d27736e4f22
root <root@debian.localhost>
2015-06-27 17:00:55 -0300

    Little ajustments

--- chapter_seeker.php
+++ chapter_seeker.php
@@ -6,17 +6,22 @@ $content_string = "";
 $chapter_title = "";
 
 function remove_unecessary_dir_el($var) {
-	if (!($var == "." || $var == "..")) {
-		return $var;
-	}
+  if (!($var == "." || $var == "..")) {
+    return $var;
+  }
 }
 
 $chapter_folders = array_filter($chapter_folders, "remove_unecessary_dir_el");
 
 foreach ($chapter_folders AS $folder) {
-	$chapter_title = file_get_contents("chapter_tests" . "/" . $folder . "/" . "title.txt");
-	$content_string = file_get_contents("chapter_tests" . "/" . $folder . "/" . "content.html");
-	$chapter = $content_start . $content_string . $bookEnd;
+
+  $chapter_title = file_get_contents("chapter_tests" . "/" . $folder . "/" . "title.txt");
+  $content_string = file_get_contents("chapter_tests" . "/" . $folder . "/" . "content.html");
+  $chapter =
+    $content_start .
+    "<h1>" . $chapter_title . "</h1>" .
+    $content_string .
+    $bookEnd;
 	
-	$book->addChapter($chapter_title, "Chapter.html", $chapter, true, EPub::EXTERNAL_REF_ADD);	
+    $book->addChapter($chapter_title, "Chapter.html", $chapter, true, EPub::EXTERNAL_REF_ADD);	
 }
