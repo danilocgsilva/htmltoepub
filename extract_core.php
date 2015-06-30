e8145e0be0feeb22299156812d3faf18ec235a81
Danilo Silva <danilocgsilva@gmail.com>
2015-06-29 22:49:37 -0300

    New fields in extract.html

--- extract_core.php
+++ extract_core.php
@@ -1,4 +1,9 @@
 <?php
-$content_output = file_get_contents('extract.iim');
-header('Content-disposition: attachment; filename=extract_script.txt');
+$goto_uri = $_GET['uri'];
+$tag_id = $_GET['id_value'];
+$file_name = $_GET['file_name'];
+
+$content_output = file_get_contents("extract.iim");
+header('Content-disposition: attachment; filename=' . $file_name);
 header('Content-type: text/plain');
+echo sprintf($content_output, $goto_uri, $tag_id, $file_name);
