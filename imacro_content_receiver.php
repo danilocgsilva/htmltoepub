7c62f2fdb23d8dab78e8ba93752a0ba0bcbd0911
Danilo Silva <danilocgsilva@gmail.com>
2015-07-16 23:17:03 -0300

    Adding new form to receive content from imacros

---
+++ imacro_content_receiver.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>IMacros content receiver</title>
+  </head>
+
+  <body>   
+  
+    <h1>IMacros content receiver</h1>
+    
+    <form>
+      <input type="hidden" id="toReceive" />
+      <input type="submit" value="Click to Send" />
+    </form>
+      
+  </body>
+</html>
