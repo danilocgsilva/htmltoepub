751493c295ed9fc98a0946f789dbb70d6b9ad4fa
Danilo Silva <danilocgsilva@gmail.com>
2015-08-04 22:29:01 -0300

    Now the widget is working!

--- cascade_widget.js
+++ cascade_widget.js
@@ -9,24 +9,40 @@ function createElementView(type, widgetID) {
         type = "Class";
     }
     
+    // Assimilaçao de elemento
     var element_view = document.getElementById(widgetID);
+    
+    // Criaçao do container do widget
     var divContainerWidget = document.createElement("div");
-    divContainerWidget.setAttribute("class", this.IDClassList.length);
+    divContainerWidget.setAttribute("ID", widgetID + "_gen_" + this.IDClassList.length);
+    
+    
+    // Text element to be between fields
+    
+    // Checks if is the first widget or not
+    // If does, it's not necessary to perform the following action
+    if (document.querySelectorAll("#" + widgetID + " > div").length != 0) {
+        var divNodeInfo = document.createElement('div');
+        divNodeInfo.setAttribute("class", "extract_field_info");
+        var nodeInfo = document.createTextNode("and after");
+        divNodeInfo.appendChild(nodeInfo);
+        divContainerWidget.appendChild(divNodeInfo);
+    }
+    
+    // Criaçao dos outros elementos constituintes do widget
     var nodeinfo = document.createTextNode(type);
     var inputText = document.createElement("input");
     var inputRemove = document.createElement("input");
     inputText.setAttribute("type", "text");
     inputRemove.setAttribute("type","button");
     inputRemove.setAttribute("value","X");
-    inputRemove.setAttribute("onClick", "this.parent.outerHTML = ''");
+    inputRemove.setAttribute("onClick", "document.getElementById('" +  widgetID + "_gen_" + this.IDClassList.length + "').outerHTML = ''");
     divContainerWidget.appendChild(nodeinfo);
     divContainerWidget.appendChild(inputText);
     divContainerWidget.appendChild(inputRemove);
     element_view.appendChild(divContainerWidget);
     
     this.IDClassList.push(divContainerWidget);
-    
-    alert(this.IDClassList.length);
 }
 
 /**
